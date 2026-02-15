dd7ffec894915807fc7f4135638cd36c910cd606
Abdikani-Ahmed0 <Faantaastik10@gmail.com>
2026-02-15 08:31:52 -0500

    added some scanners and activated folders

---
+++ 04-scanners/portscanner.py
@@ -0,0 +1,38 @@
+import sys
+import socket
+from datetime import datetime as dt
+
+# define our target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #translate hostname to IPv4
+else:
+	print("Invalid amount of arguments. ")
+	print("Syntax: python3 portscanner.py <ip>")
+	
+# add a pretty banner
+print("-" *50)
+print("Scanning target " + target)
+print("Time started: " + str(dt.now()))
+print("-" *50)
+
+try:
+	for port in range(1, 65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target, port))
+		if result == 0:
+	        	print("Port {} is Open".format(port))
+	s.close()
+	
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+	
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+	
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
+	
